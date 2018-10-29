56a7333d81b51163743a2ea0bdde629c28d7c796
anjanimantri <anjanim@cybage.com>
2018-10-29 15:59:49 +0530

    Add changes

--- FromMac/Anjani/src/components/components.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ExpandableFaQsComponent } from './expandable-fa-qs/expandable-fa-qs';
-
-@NgModule({
-	declarations: [ExpandableFaQsComponent],
-	imports: [],
-	exports: [ExpandableFaQsComponent]
-})
-export class ComponentsModule {}
